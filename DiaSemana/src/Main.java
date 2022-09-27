60c754a96b19fb0b737ea39293e98c9350688c91
Hejociara do Vale <hejociarav@gmail.com>
2022-09-27 18:00:22 -0300

    "Feature"

--- DiaSemana/src/Main.java
+++ DiaSemana/src/Main.java
@@ -13,8 +13,24 @@ public class Main {
 
         if(diaSemana.equalsIgnoreCase("domingo"))
             System.out.println(1);
-            
+        else if (diaSemana.equalsIgnoreCase("lunes"))
+            System.out.println(2);
+        else if (diaSemana.equalsIgnoreCase("Martes"))
+            System.out.println(3);
+        else if (diaSemana.equalsIgnoreCase("Miércoles")
+                || diaSemana.equalsIgnoreCase("miercoles"))
+            System.out.println(4);
+        else if (diaSemana.equalsIgnoreCase("Jueves"))
+            System.out.println(5);
+        else if (diaSemana.equalsIgnoreCase("Viernes"))
+            System.out.println(6);
+        else if (diaSemana.equalsIgnoreCase("Sábado")
+                || diaSemana.equalsIgnoreCase("sabado"))
+            System.out.println(7);
+        else {
+            System.out.println("Día inválido");
         }
+
 
         entrada.close();
 
